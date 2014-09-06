e90c3244b5c14636fb11b36fb8428566cb7f165c
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-09-06 21:04:20 +0200

    implementing votes and saving in localStorage

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -4,7 +4,18 @@ var app = angular.module("playit", ['websocket', 'ui.bootstrap']);
 app.controller('VideofeedCtrl', function($scope, $websocket, $rootScope) {
 	var websocket = $websocket.connect('ws://localhost:8888/ws/action');
 	$scope.mediaitems = [];
+	$scope.votes = {};
 	$scope.selected = -1;
+
+	if (window.localStorage) {
+		$scope.votes = JSON.parse(window.localStorage.getItem('votes')) || {};
+	}
+
+	function send(topic, body) {
+		body = body || {};
+		body.token = PlayIT.get_cookie('chalmersItAuth');
+		websocket.emit(topic, body);
+	}
 
 	websocket.register('queue/update', function(topic, body) {
 		console.log('queue:', body);
@@ -19,6 +30,11 @@ app.controller('VideofeedCtrl', function($scope, $websocket, $rootScope) {
 			type: 'danger',
 			message: 'Failed to save: ' + body.split('due to: ')[1]
 		});
+	});
+
+	websocket.register('media_item/new', function(topic, body) {
+		$scope.votes[body.id] = 0;
+		saveVotes($scope);
 	});
 
 	$scope.time_format = function(seconds) {
@@ -59,6 +75,17 @@ app.controller('VideofeedCtrl', function($scope, $websocket, $rootScope) {
 		$scope.selected = index; 
 	};
 
+	$scope.get_weight = function(index) {
+		var saved_value = $scope.votes[$scope.mediaitems[index].id] || 0;
+		return saved_value + $scope.mediaitems[index].value;
+	};
+
+	function saveVotes($scope) {
+		if (window.localStorage) {
+			window.localStorage.setItem('votes', JSON.stringify($scope.votes))
+		}
+	}
+
 	function nextItem() {
 		var next = $scope.selected + 1;
 		$scope.selected = Math.min(next, $scope.mediaitems.length - 1);
@@ -72,31 +99,42 @@ app.controller('VideofeedCtrl', function($scope, $websocket, $rootScope) {
 	$scope.upvoteItem = function(index) {
 		index = index || $scope.selected;
 		var item = $scope.mediaitems[$scope.selected];
-		item.value++;
-	}
+		if ($scope.votes[item.id] !== 1 && $scope.votes[item.id] !== 0) {
+			$scope.votes[item.id] = 1;
+			saveVotes($scope);
+			send('add_vote', {
+				id: item.external_id,
+				type: item.type
+			});
+		}
+	};
 	$scope.downvoteItem = function(index) {
 		index = index || $scope.selected;
 		var item = $scope.mediaitems[$scope.selected];
-		item.value--;
-	}
+		if ($scope.votes[item.id] != -1) {
+			$scope.votes[item.id] = -1;
+			saveVotes($scope);
+			send('remove_vote', {
+				id: item.external_id,
+				type: item.type
+			});
+		}
+	};
 
 	$rootScope.$on('add_item', function(event, args) {
 		var mediaItem = args;
 		args.token = PlayIT.get_cookie('chalmersItAuth');
-
-		websocket.emit('add_item', mediaItem);
+		send('add_item', mediaItem);
 	});
 
-	websocket.emit('get_queue', {});
-
 	var defaultCallback = Mousetrap.stopCallback;
-		Mousetrap.stopCallback = function(e, element, combo) {
+	Mousetrap.stopCallback = function(e, element, combo) {
 		if (combo === 'esc') {
 			return false;
 		} else {
 			return defaultCallback(e, element, combo);
 		}
-	}
+	};
 	Mousetrap.bind('/', function(e){
 		document.getElementById('insert_video').focus();
 		e.preventDefault();
@@ -116,6 +154,9 @@ app.controller('VideofeedCtrl', function($scope, $websocket, $rootScope) {
 	Mousetrap.bind('z', function(){
 		$scope.$apply($scope.downvoteItem);
 	});
+
+	// startup the webapp with a request to fetch the current queue
+	send('get_queue');
 });
 
 app.directive('nowPlaying', function() {
