4bb8753eb4cab3bb6dd7a50f06d0b2ef30a74fee
Emil Edholm <emil@edholm.it>
2014-10-08 03:19:37 +0200

    Added start script and desktop file for the playback client

---
+++ playbackClient/start.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+if pgrep playIT_client.py; then
+    echo "Client already running!"
+    pkill playIT_client.py
+fi
+
+# Fetch new version of client
+url="https://raw.githubusercontent.com/cthit/playIT-python/master/playbackClient/playIT_client.py"
+if wget $url -q -O ~/playIT_client.py
+    then
+    chmod +x ~/playIT_client.py
+
+    # Is mopidy running?
+    if ! pgrep mopidy >/dev/null
+        then
+        echo "Starting mopidy"
+        mopidy >/dev/null 2>&1 &
+    fi
+    ~/playIT_client.py
+    # Kill my own children
+    [[ -z "$(jobs -p)" ]] || kill $(jobs -p)
+fi
