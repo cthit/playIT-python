b3bc51061fe4b75067b32ce2c61682c74c260dee
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-02-26 02:34:34 +0100

    Refactor NowPlaying component

--- frontend/src/components/NowPlaying.js
+++ frontend/src/components/NowPlaying.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import Helpers from "../lib/helpers.js";
 
-export default class NowPlaying extends Component {
-  render() {
-    let item = this.props.item;
-    if (!item) {
-      return (<div className="now-playing"><span>Nothing playing right now...</span></div>);
-    } else {
-      return (
-        <div className="now-playing">
-          <span>Now playing: <a href={Helpers.get_link(item)}>{item.title}</a> [{Helpers.format_time(item.duration)}] - {item.author} <em>Queued by {item.nick}</em></span>
-        </div>
-      );
-    }
+const NowPlaying = ({ track }) => {
+  if (!track) {
+    return (
+      <div className="now-playing"><span>Nothing playing right now...</span></div>
+    )
+  } else {
+    return (
+      <div className="now-playing">
+        <span>Now playing: <a href={Helpers.get_link(item)}>{item.title}</a> [{Helpers.format_time(item.duration)}] - {item.author} <em>Queued by {item.nick}</em></span>
+      </div>
+    )
   }
 }
+
+export default NowPlaying
