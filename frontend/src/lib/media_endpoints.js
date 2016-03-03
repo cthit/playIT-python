7c54d4ebdb1b50da0662cfaebe1ca7c7d6f00a84
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-03-04 00:50:51 +0100

    Refactor mediaItems fetcher

--- frontend/src/lib/media_endpoints.js
+++ frontend/src/lib/media_endpoints.js
@@ -6,23 +6,19 @@ if (!window.fetch) {
 
 window.urlparams = urlparams;
 
-var regexes = {
+const regexes = {
   youtube: {index: 2, regex: /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]+).*/},
   spotify: {index: 3, regex: /^(.*open\.)?spotify(\.com)?[\/:]track[\/:]([a-zA-Z0-9]+)$/},
   soundcloud: {index: 1, regex: /^.*soundcloud.com\/([a-zA-Z0-9-\/]+)/}
 };
 
+const fetchJson = (url, params) => fetch(url + '?' + urlparams(params)).then(resp => resp.json());
+
 const RESULT_LIMIT = 20;
 
-function urlparams(params) {
-  return Object.keys(params).map((k) => k + '=' + params[k]).join('&');
-}
+const urlparams = (params) => Object.keys(params).map((k) => k + '=' + params[k]).join('&')
 
 export default class MediaEndpoints {
-  constructor() {
-
-  }
-
   isURL(string) {
     return Object.keys(regexes).filter((k) => regexes[k].regex.test(string)).length > 0;
   }
@@ -35,30 +31,27 @@ export default class MediaEndpoints {
   }
 
   search_youtube(query) {
-    return fetch('https://www.googleapis.com/youtube/v3/search?' + urlparams({
+    return fetchJson('https://www.googleapis.com/youtube/v3/search', {
       part: 'snippet',
       key: 'AIzaSyD2v5xyu8uXd6ER0xo35RWxfFLlTXezXZA',
       maxResults: RESULT_LIMIT,
       type: 'video',
       q: query
-    })).then((response) => response.json()).then((json) => json.items.map((video) => {
-      let snippet = video.snippet;
-      return {
+    }).then((json) => json.items.map(({ snippet, id: { videoId } }) => ({
         name: snippet.title,
         thumbnail: snippet.thumbnails.default.url,
         author: snippet.channelTitle,
         type: 'youtube',
-        id: video.id.videoId
-      };
-    }));
+        id: videoId
+    })));
   }
 
   search_soundcloud(query) {
-    return fetch('http://api.soundcloud.com/tracks?' + urlparams({
+    return fetchJson('http://api.soundcloud.com/tracks', {
       client_id: 'a2cfca0784004b38b85829ba183327cb',
       limit: RESULT_LIMIT,
       q: query
-    })).then((response) => response.json()).then((json) => json.map((track) => ({
+    }).then((json) => json.map((track) => ({
         name: track.title,
         thumbnail: track.artwork_url,
         author: track.user.username,
@@ -68,12 +61,12 @@ export default class MediaEndpoints {
   }
 
   search_spotify(query) {
-    return fetch('https://api.spotify.com/v1/search?' + urlparams({
+    return fetchJson('https://api.spotify.com/v1/search', {
       limit: RESULT_LIMIT,
       type: 'track',
       q: query
-    })).then((response) => response.json()).then((json) => json.tracks.items.map((track) => {
-      let image = track.album.images[0];
+    }).then((json) => json.tracks.items.map((track) => {
+      const [image] = track.album.images;
       return {
         name: track.name,
         thumbnail: (image && image.url) || null,
