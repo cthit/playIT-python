3ee268b96c12c58f73fda72d713725a5f4928492
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-03-09 23:07:52 +0100

    Fix depicated call

--- frontend/src/components/ResultItem.js
+++ frontend/src/components/ResultItem.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 export default class ResultItem extends Component {
   componentDidUpdate() {
     if (this.props.selected) {
-      React.findDOMNode(this).scrollIntoView(false);
+      ReactDOM.findDOMNode(this).scrollIntoView(false);
     }
   }
   clickEvent(event) {
