8d207add556a6eab004fec141ec528e14bf96107
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-03-24 01:52:25 +0100

    VideoFeedContainer mapping dispatch

--- frontend/src/components/ActiveVideoFeed.js
+++ frontend/src/components/ActiveVideoFeed.js
@@ -7,33 +7,32 @@ import VideoFeed from './VideoFeed'
 
 
 const mapStateToProps = (state) => ({
+  activeFeedId: state.main.show,
   items: state[state.main.show].items,
   selectedId: state[state.main.show].selectedId
 })
 
-const mapDispatchToProps = (dispatch, props) => {
-  if (props.activeFeedId === 'tracks') {
-    return {
-      onUpvote: (track) => {
-        dispatch(trackActions.upvoteTrack(track))
-      },
-      onDownvote: (track) => {
-        dispatch(trackActions.downvoteTrack(track))
-      }
-    }
-  } else {
-    return {
-      onUpvote: (playlist) => {
-        dispatch(playlistActions.upvotePlaylist(playlist))
-      },
-      onDownvote: (playlist) => {
-        dispatch(playlistActions.downvotePlaylist(playlist))
-      }
-    }
+const mapDispatchToProps = (dispatch) => ({
+  tracks: {
+    onUpvote: (track) => dispatch(trackActions.upvoteTrack(track)),
+    onDownvote: (track) => dispatch(trackActions.downvoteTrack(track)),
+    onClickItem: (track) => dispatch(trackActions.setFeedNavigate(track.id))
+  },
+  playlists: {
+    onUpvote: (playlist) => dispatch(playlistActions.upvotePlaylist(playlist)),
+    onDownvote: (playlist) => dispatch(playlistActions.downvotePlaylist(playlist)),
+    onClickItem: (playlist) => dispatch(playlistActions.setFeedNavigate(playlist.id))
   }
-}
+})
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+  ...ownProps,
+  ...stateProps,
+  ...dispatchProps[stateProps.activeFeedId]
+});
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
+  mergeProps
 )(VideoFeed)
