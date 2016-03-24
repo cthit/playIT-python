f8313d88a72ece1e22f849e943df1031c264e1a0
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-03-24 01:27:13 +0100

    Scroll to VideoItem when active

--- frontend/src/components/VideoItem.js
+++ frontend/src/components/VideoItem.js
@@ -1,45 +1,44 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import VotingArrows from "./VotingArrows";
 import Helpers from "../lib/helpers";
 
-const VideoItem = ({ item, onUpvote, onDownvote, active }) => {
-  let classes = ['media', item.type];
-  if (active) {
-    classes.push("selected")
+const VideoItem = React.createClass({
+  componentDidUpdate() {
+    if (this.props.active) {
+      ReactDOM.findDOMNode(this).scrollIntoView(true);
+    }
+  },
+  propTypes: {
+    item: React.PropTypes.shape({
+      id: React.PropTypes.any.isRequired,
+      user_vote: React.PropTypes.number,
+      value: React.PropTypes.number.isRequired
+    }).isRequired,
+    onUpvote: React.PropTypes.func.isRequired,
+    onDownvote: React.PropTypes.func.isRequired,
+    active: React.PropTypes.bool.isRequired
+  },
+  render() {
+    const { item, onUpvote, onDownvote, active } = this.props;
+    const itemIsPlaylist = item.type.indexOf('_list') !== -1;
+
+    return (
+      <li className={['media', item.type, (active ? 'selected' : '')].join(' ')}>
+        <VotingArrows {...item} onUpvote={onUpvote} onDownvote={onDownvote} />
+        <div className="image">
+          <a href={Helpers.get_link(item)} target="_blank">
+            <img src={item.thumbnail} alt={item.external_id} />
+          </a>
+        </div>
+        <div className="info">
+          <h3>{item.title} {itemIsPlaylist && '[' + Helpers.format_time(item.duration) + ']'}</h3>
+          <small><strong>{item.author}</strong> — Added by: <span title={item.cid}>{item.nick}</span></small>
+          <p>{item.description}</p>
+        </div>
+      </li>
+    )
   }
-  const className = classes.join(' ')
-
-  let duration;
-  if (item.type.indexOf('_list') != -1) {
-    duration = '[' + Helpers.format_time(item.duration) + ']';
-  }
-
-  return (
-    <li className={className}>
-      <VotingArrows {...item} onUpvote={onUpvote} onDownvote={onDownvote} />
-      <div className="image">
-        <a href={Helpers.get_link(item)} target="_blank">
-          <img src={item.thumbnail} alt={item.external_id} />
-        </a>
-      </div>
-      <div className="info">
-        <h3>{item.title} {duration}</h3>
-        <small><strong>{item.author}</strong> — Added by: <span title={item.cid}>{item.nick}</span></small>
-        <p>{item.description}</p>
-      </div>
-    </li>
-  );
-}
-
-VideoItem.propTypes = {
-  item: React.PropTypes.shape({
-    id: React.PropTypes.any.isRequired,
-    user_vote: React.PropTypes.number,
-    value: React.PropTypes.number.isRequired
-  }).isRequired,
-  onUpvote: React.PropTypes.func.isRequired,
-  onDownvote: React.PropTypes.func.isRequired,
-  active: React.PropTypes.bool.isRequired
-}
+});
 
 export default VideoItem
