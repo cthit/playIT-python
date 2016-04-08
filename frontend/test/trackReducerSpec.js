85776d30fa7e411fd23f9bd6adc9e9889706e3a5
André Samuelsson <andresamuelsson94@gmail.com>
2016-04-09 01:22:04 +0200

    Add test for receiveItemSuccess

---
+++ frontend/test/trackReducerSpec.js
@@ -0,0 +1,21 @@
+import expect from 'expect'
+
+import * as trackActions from '../src/actions/trackActions'
+import trackReducer from '../src/store/trackReducer'
+
+describe('trackReducer', () => {
+  it("should add track to items", () => {
+    const state = {items: []}
+    const track = {
+      type: "bapedibop",
+      value: 0,
+      id: 5
+    }
+    expect(
+      trackReducer(state, trackActions.receiveItemSuccess(track))
+    ).toEqual({
+      items: [track],
+      selectedId: 5
+    })
+  });
+});
