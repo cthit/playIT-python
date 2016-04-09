07159ef80667fe49fa019fd1ef17e9564f56d261
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-04-09 18:09:20 +0200

    Fix bug for user not having devtools installed

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -11,7 +11,7 @@ const store = createStore(
   compose(
     applyMiddleware(createSagaMiddleware(...searchBoxSaga)),
     autoRehydrate(),
-    window.devToolsExtension ? window.devToolsExtension() : null
+    window.devToolsExtension ? window.devToolsExtension() : f => f
   )
 );
 
