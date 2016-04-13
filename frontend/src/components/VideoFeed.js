0e2952e030532cc72f54da3def983c7eb01c88e7
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-04-13 23:54:17 +0200

    Enter, leave transitions solves #25

--- frontend/src/components/VideoFeed.js
+++ frontend/src/components/VideoFeed.js
@@ -1,18 +1,21 @@
 import React from "react"
 import VideoItem from "./VideoItem"
+import ReactCSSTransitionGroup from "react-addons-css-transition-group"
 
 
 const VideoFeed = ({ items, onUpvote, onClickItem, onDownvote, selectedId }) => (
   <ol className="view-feed">
-    {items.map(item => (
-      <VideoItem key={item.id}
-                 item={item}
-                 onClick={() => onClickItem(item)}
-                 active={selectedId === item.id}
-                 onUpvote={() => onUpvote(item)}
-                 onDownvote={() => onDownvote(item)} />
-    ))}
-    {!items.length && <li className="error-item">No items in queue</li>}
+    <ReactCSSTransitionGroup transitionName="swipe" transitionAppear={false} transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+      {items.map(item => (
+        <VideoItem key={item.id}
+                   item={item}
+                   onClick={() => onClickItem(item)}
+                   active={selectedId === item.id}
+                   onUpvote={() => onUpvote(item)}
+                   onDownvote={() => onDownvote(item)} />
+      ))}
+      {!items.length && <li className="error-item">No items in queue</li>}
+    </ReactCSSTransitionGroup>
   </ol>
 );
 
